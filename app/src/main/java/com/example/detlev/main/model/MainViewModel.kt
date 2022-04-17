42fde957444132ae57b822a38c5909e284828a07
Detlev Noll <dn@experikus.de>
2022-04-17 09:16:08 +0200

    Daten abrufen Errorhandling tmp

--- app/src/main/java/com/example/detlev/main/model/MainViewModel.kt
+++ app/src/main/java/com/example/detlev/main/model/MainViewModel.kt
@@ -2,10 +2,7 @@ package com.example.detlev.main.model
 
 import android.provider.Settings.Global.getString
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.detlev.main.network.FitnessApi
 import com.example.detlev.main.network.FitnessData
 import kotlinx.coroutines.launch
@@ -13,31 +10,33 @@ import org.json.JSONException
 import org.json.JSONObject
 
 class MainViewModel : ViewModel() {
-    private var _fitnessData = MutableLiveData<FitnessData>()
+    private var _fitnessData = MutableLiveData<String>()
     val fitnessData: LiveData<FitnessData>
-        get() = _fitnessData
+        = Transformations.map(_fitnessData) { parseJsonData(it) }
 
     private val TAG = "MainViewModel"
 
 
     fun getFitnessData() {
+        Log.i(TAG, "entering getFitnessData()")
         // Daten im Hintergrund abrufen
         try {
             viewModelScope.launch {
-                val jsonString = FitnessApi.retrofitService.getData()
-                // Daten umwandeln
-                parseJsonData(jsonString)
+                _fitnessData.value = FitnessApi.retrofitService.getData()
             }
         }  catch (e: Exception) {
             Log.i(TAG, "Error loading data ${e.message}$")
+            _fitnessData.value = ""
         }
+
     }
 
-    private fun parseJsonData(jsonString: String) {
+    private fun parseJsonData(jsonString: String) : FitnessData {
+        Log.i(TAG, "entering getFitnessData()")
         try {
             //response String zu einem JSON Objekt
             val obj = JSONObject(jsonString)
-            _fitnessData.value =  FitnessData(
+            return  FitnessData(
                 // Einzelne Elemente des JSON Objects extrahieren
                 fitness = obj.getDouble("fitness"),
                 puls = obj.getInt("puls"),
@@ -46,6 +45,7 @@ class MainViewModel : ViewModel() {
         } catch (e : JSONException) {
             e.printStackTrace()
             Log.i(TAG, "Error JSON Parsing ${e.message}")
+            return FitnessData(0.0, 0, "")
         }
     }
 }
